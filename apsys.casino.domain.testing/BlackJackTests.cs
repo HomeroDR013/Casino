f610043c521328c68ebd1f3d213c03e885f7cd46
HomeroDR013 <86341513+HomeroDR013@users.noreply.github.com>
2021-09-16 18:30:58 -0500

    Created the BlackJack.cs and BlackJackTests.cs classes, with the BuildDeck method and its test, which creates a deck to play BlackJack

---
+++ apsys.casino.domain.testing/BlackJackTests.cs
@@ -0,0 +1,32 @@
+﻿using apsys.casino.domain.Shared;
+using NUnit.Framework;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace apsys.casino.domain.testing
+{
+    class BlackJackTests
+    {
+        [Test]
+        public void BuildDeck_ReturnvalidDeck()
+        {
+            //Arrange
+            BlackJack blackJack = new BlackJack();
+            //Act
+            IEnumerable<Card> deck = blackJack.BuildDeck();
+            //Assert
+            Assert.NotNull(deck);
+            Assert.That(deck.Count(), Is.EqualTo(52));
+            foreach (var cardSuit in Constants.SuitList)
+            {
+                foreach (var cardValue in Constants.ValueList)
+                {
+                    Assert.That(deck.Count(c => (c.Suit == cardSuit) && (c.Value == cardValue)), Is.EqualTo(1));
+                }
+            }
+            Assert.That(deck.Count(), Is.EqualTo(52));
+        }
+    }
+}
---
+++ apsys.casino.domain/BlackJack.cs
@@ -0,0 +1,21 @@
+﻿using apsys.casino.domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace apsys.casino.domain
+{
+    public class BlackJack
+    {
+        public IEnumerable<Card> BuildDeck()
+        {
+            IList<Card> cards = new List<Card>();
+            foreach (var cardSuit in Constants.SuitList)
+            {
+                foreach (var cardValue in Constants.ValueList)
+                    cards.Add(new Card { Suit = cardSuit, Value = cardValue });
+            }
+            return cards;
+        }
+    }
+}
